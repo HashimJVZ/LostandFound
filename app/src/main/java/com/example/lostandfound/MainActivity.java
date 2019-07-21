f5308f3f9feb205bad8b16757c41098e4ea9928a
Hashim PC <highbrowdirector@gmail.com>
2019-07-21 20:01:23 +0530

    added lost and found activity, problem with R.id

--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.lostandfound;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button lostbtn = findViewById(R.id.lostbtn);
+        Button foundbtn = findViewById(R.id.foundbtn);
+
+      lostbtn.setOnClickListener(new Button.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              startActivity(LostActivity);
+          }
+      });
+
+      foundbtn.setOnClickListener(new Button.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              startActivity(FoundActivity);
+          }
+      });
     }
 }
