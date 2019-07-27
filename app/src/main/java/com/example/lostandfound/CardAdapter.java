eaacad3df4031d41ff37d01ddbfa235bd8ae38f6
HashimJVZ <highbrowdirector@gmail.com>
2019-07-27 09:25:39 +0530

    some Haseeb things

---
+++ app/src/main/java/com/example/lostandfound/CardAdapter.java
@@ -0,0 +1,60 @@
+package com.example.lostandfound;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lostandfound.model.CardData;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyViewHolder> {
+
+    private Context context;
+    List<CardData> cardData;
+
+    public CardAdapter(Context context, List<CardData> cardData) {
+        this.context = context;
+        this.cardData = cardData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.card, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Picasso.get().load(cardData.get(position).getImage()).into(holder.imageView);
+        holder.title.setText(cardData.get(position).getTitle());
+        holder.description.setText(cardData.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardData.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView title, description;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.cardimage);
+            title = itemView.findViewById(R.id.cardtitle);
+            description = itemView.findViewById(R.id.carddescription);
+        }
+    }
+}
--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -1,40 +1,36 @@
 package com.example.lostandfound;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ListView listView;
+    private String[] districts = {"Alappuzha", "Ernakulam", "Idukki", "Kannur", "Kasaragod", "Kollam", "Kottayam","Kozhikode", "Malappuram", "Palakkad", "Pathanamthitta", "Thiruvananthapuram", "Thrissur", "Wayanad"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, districts);
 
-      Button lostbtn, foundbtn;
+        listView = findViewById(R.id.listview);
 
-      lostbtn = findViewById(R.id.lostbtn);
-      lostbtn.setOnClickListener(new Button.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Intent intent = new Intent(MainActivity.this, LostActivity.class);
-              startActivity(intent);
-          }
-      });
+        listView.setAdapter(arrayAdapter);
 
-      foundbtn = findViewById(R.id.foundbtn);
-      foundbtn.setOnClickListener(new Button.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Intent intent = new Intent(MainActivity.this, FoundActivity.class);
-              startActivity(intent);
-          }
-      });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Clicked " + districts[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
